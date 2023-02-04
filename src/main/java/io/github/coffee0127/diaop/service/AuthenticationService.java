3c3c862cc57af1e1d283a47d1e9955d23a0719cf
Bo-Xuan Fan <coffee0127@gmail.com>
2023-02-04 20:39:14 +0800

    refactor: extract method for Composed Method to align sequence diagram

--- src/main/java/io/github/coffee0127/diaop/service/AuthenticationService.java
+++ src/main/java/io/github/coffee0127/diaop/service/AuthenticationService.java
@@ -12,55 +12,81 @@ public class AuthenticationService {
   public boolean verify(String account, String password, String otp) {
     var httpClient = new HttpClient("https://joey.com");
 
-    var isLockedResponse =
-        httpClient.get("/api/failedCounter/isLocked?account=" + account, Boolean.class);
-    isLockedResponse.ensureSuccessStatusCode();
-    if (isLockedResponse.read()) {
+    var isLocked = isLocked(account, httpClient);
+    if (isLocked) {
       throw new FailedTooManyTimesException(account);
     }
 
-    // get password from DB
+    var passwordFromDb = getPasswordFromDb(account);
+
+    var hashedPassword = getHashedPassword(password);
+
+    var currentOtp = getCurrentOtp(account, httpClient);
+
+    // check valid
+    if (passwordFromDb.equals(hashedPassword) && currentOtp.equals(otp)) {
+      resetFailedCount(account, httpClient);
+      return true;
+    } else {
+      addFailedCount(account, httpClient);
+
+      logFailedCount(account, httpClient);
+
+      var message = "account:" + account + " try to login failed";
+      notify(message);
+      return false;
+    }
+  }
+
+  private boolean isLocked(String account, HttpClient httpClient) {
+    var isLockedResponse =
+        httpClient.get("/api/failedCounter/isLocked?account=" + account, Boolean.class);
+    isLockedResponse.ensureSuccessStatusCode();
+    return isLockedResponse.read();
+  }
+
+  private String getPasswordFromDb(String account) {
     String passwordFromDb;
     try (var connection = new SqlConnection("jdbc:h2:mem:my_app")) {
       passwordFromDb = connection.query("GET_PASSWORD_SQL", Map.of("ID", account), String.class);
     }
+    return passwordFromDb;
+  }
 
-    // hash input password
-    var hashedPassword = DigestUtils.sha256Hex(password);
+  private String getHashedPassword(String password) {
+    return DigestUtils.sha256Hex(password);
+  }
 
-    // get current otp
+  private String getCurrentOtp(String account, HttpClient httpClient) {
     var response = httpClient.get("/api/otps?account" + account, String.class);
     if (response.isSuccessStatusCode()) {
     } else {
       throw new RuntimeException("web api error, accountId:" + account);
     }
-    var currentOtp = response.read();
+    return response.read();
+  }
 
-    // check valid
-    if (passwordFromDb.equals(hashedPassword) && currentOtp.equals(otp)) {
-      // reset the failed counter
-      var resetResponse =
-          httpClient.post("/api/failedCounter/reset?account=" + account, Void.class);
-      resetResponse.ensureSuccessStatusCode();
-      return true;
-    } else {
-      // add the failed counter
-      var addFailedCountResponse =
-          httpClient.post("/api/failedCounter/add?account=" + account, Void.class);
-      addFailedCountResponse.ensureSuccessStatusCode();
+  private void resetFailedCount(String account, HttpClient httpClient) {
+    var resetResponse = httpClient.post("/api/failedCounter/reset?account=" + account, Void.class);
+    resetResponse.ensureSuccessStatusCode();
+  }
 
-      // log the account's failed count
-      var failedCountResponse =
-          httpClient.post("/api/failedCounter/getFailedCount?account=" + account, Integer.class);
-      failedCountResponse.ensureSuccessStatusCode();
-      var logger = Logger.getLogger("MyLogger");
-      logger.info("accountId:" + account + " failed times:" + failedCountResponse.read());
+  private void addFailedCount(String account, HttpClient httpClient) {
+    var addFailedCountResponse =
+        httpClient.post("/api/failedCounter/add?account=" + account, Void.class);
+    addFailedCountResponse.ensureSuccessStatusCode();
+  }
 
-      var message = "account:" + account + " try to login failed";
-      var slackClient = new SlackClient("<YOUR_API_TOKEN>");
-      slackClient.postMessage("#my-channel", message);
+  private void logFailedCount(String account, HttpClient httpClient) {
+    var failedCountResponse =
+        httpClient.post("/api/failedCounter/getFailedCount?account=" + account, Integer.class);
+    failedCountResponse.ensureSuccessStatusCode();
+    var logger = Logger.getLogger("MyLogger");
+    logger.info("accountId:" + account + " failed times:" + failedCountResponse.read());
+  }
 
-      return false;
-    }
+  private void notify(String message) {
+    var slackClient = new SlackClient("<YOUR_API_TOKEN>");
+    slackClient.postMessage("#my-channel", message);
   }
 }
